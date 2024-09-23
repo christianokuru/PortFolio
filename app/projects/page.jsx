c063b5c4d86054dd53ba2408304fe47571c09c71
christianokuru <okuruchristian@gmail.com>
2024-09-23 09:25:39 +0100

    Added a new project and made some minor changes to imrpove the UI/UX

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import project1 from "@/public/projects/ui-ux-design.png";
+import project2 from "@/public/projects/portfolio.png";
 import { ArrowRightCircle, Github } from "lucide-react";
 import Link from "next/link";
 
@@ -8,13 +9,25 @@ const projects = [
     id: 1,
     image: project1,
     title: "Frontend Project",
-    description: "The UI/UX team created the design of this website. I designed the website utilizing the mobile first strategy, however it has no functionality or pages.",
+    description:
+      "The UI/UX team created the design of this website. I designed the website utilizing the mobile first strategy, however it has no functionality or pages.",
     links: {
-      live: "https://nextjs2-9g16q0pd1-okuruchristian-gmailcoms-projects.vercel.app/",
+      live: "https://nextjs2-okuruchristian-gmailcoms-projects.vercel.app/",
       github: "https://google.com",
     },
+    stack: ['Next.js', 'Tailwind', 'Figma'],
   },
-  // Add more projects as needed
+  {
+    id: 2,
+    image: project2,
+    title: "My Portfolio Site",
+    description: "This is my personal portfolio site",
+    links: {
+      live: "port-folio-two-nu.vercel.app",
+      github: "https://github.com/christianokuru/PortFolio",
+    },
+    stack: ['Next.js', 'Tailwind', 'Shadcn']
+  },
 ];
 
 export default function Projects() {
@@ -27,14 +40,24 @@ export default function Projects() {
           </h2>
         </div>
 
-
         {/* Looping over the Project array of objects */}
         {projects.map((project) => (
-          <div key={project.id} className="flex lg:flex-row flex-col lg:border lg:border-border lg:rounded-md mb-4">
+          <div
+            key={project.id}
+            className="flex lg:flex-row flex-col lg:border lg:border-border lg:rounded-md mb-4"
+          >
             {/* Image Section */}
             <div className="border-r lg:w-1/2 p-4">
-              <Link href={project.links.live} target="_blank" aria-label={`View ${project.title}`}>
-                <Image src={project.image} alt={project.title} className="w-full h-auto rounded-sm" />
+              <Link
+                href={project.links.live}
+                target="_blank"
+                aria-label={`View ${project.title}`}
+              >
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-auto rounded-sm"
+                />
               </Link>
             </div>
 
@@ -52,18 +75,34 @@ export default function Projects() {
 
               {/* Stack Section (you can customize this as needed) */}
               <div className="flex border-b-2 items-center gap-x-3 w-full mb-3 pb-2">
-                <p className="leading-7">Next.js,</p>
-                <p className="leading-7">Tailwind CSS,</p>
-                <p className="leading-7">Figma</p>
+                {project.stack.map((tech, index) => (
+                  <p key={index} className="relative border border-x-ring rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">{tech}</p>    
+                ))}
               </div>
 
               {/* Links Section */}
-              <div className="flex gap-3 lg:mt-10">
-                <Link href={project.links.live} target="_blank" aria-label={`Visit ${project.title}`}>
-                  <ArrowRightCircle className="text-green-600 hover:text-green-800 transition" width={40} height={40} />
+              <div className="flex gap-3 lg:mt-5">
+                <Link
+                  href={project.links.live}
+                  target="_blank"
+                  aria-label={`Visit ${project.title}`}
+                >
+                  <ArrowRightCircle
+                    className="text-green-600 hover:text-green-800 transition"
+                    width={30}
+                    height={30}
+                  />
                 </Link>
-                <Link href={project.links.github} target="_blank" aria-label={`View ${project.title} GitHub Repository`}>
-                  <Github className="text-purple-600 hover:text-purple-800 transition" width={40} height={40} />
+                <Link
+                  href={project.links.github}
+                  target="_blank"
+                  aria-label={`View ${project.title} GitHub Repository`}
+                >
+                  <Github
+                    className="text-purple-600 hover:text-purple-800 transition"
+                    width={30}
+                    height={30}
+                  />
                 </Link>
               </div>
             </article>
